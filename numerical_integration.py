e29f088f29bafa0252a4e2193ae6a3d133479985
Taylor Pytel <90528316+tpyte001@users.noreply.github.com>
2022-09-22 13:36:24 -0400

    laptopwork

--- numerical_integration.py
+++ numerical_integration.py
@@ -4,5 +4,10 @@
 # split into N trapezoids
 # area of trapezoid = h/2 (A+B)
 # so h would be (b-a) / N
-# so sum of (b-a(/N
+# so sum of (b-a)/N
 # Integral = sum of area of ith trapezoid
+import numpy as np
+import math as m
+
+def integral_sin(a="lowerbound",b="upperbound",n,h):
+    area = (h / 2) * ((np.sin(a) + np.sin(b))) + h 
