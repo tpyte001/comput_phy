aaeb1058f1137e6a7c4fdd46c3f793b2f9634289
tpyte001 <tpyte001@plattsburgh.edu>
2022-09-28 12:05:28 -0400

    finished hw

--- phyHW3.py
+++ phyHW3.py
@@ -4,22 +4,27 @@ import numpy as np
 x = np.pi/3
 
 #forward difference of sin(x)
+
 def fwdDiffSin(x,h=0.01):
     return (np.sin(x+h) - np.sin(x)) / h
 
 #Central difference of sin(x)
+
 def cenDiffSin(x,h=0.01):
     return (np.sin(x+h) - np.sin(x-h)) / (2*h)
 
 #test
+
 print(fwdDiffSin(x))
 print(cenDiffSin(x))
 
 #Percent error of central difference to forward difference
+
 error = ((cenDiffSin(x) - fwdDiffSin(x)) / cenDiffSin(x)) * 100
 print(error,'Percent') 
 
 #Error comparison for different values of h, increasing
+
 def deltah(x):
     errorOne = ((cenDiffSin(x,0.00001) - fwdDiffSin(x,0.00001)) / cenDiffSin(x,0.00001)) * 100
     errorTwo = ((cenDiffSin(x,0.0001) - fwdDiffSin(x,0.0001)) / cenDiffSin(x,0.0001)) * 100
@@ -35,9 +40,19 @@ def deltah(x):
     #make into a graph
 print('Percent Accuracy')
 deltah(x)
+print('-----------------------------------------')
 
 #finite difference for second derivative
-def second_derivative_sin(x):
-    return (sin(x+h)-2*sin(x)+sin(x-h)) / (2*h)
-    
 
+def second_derivative_sin(x,h=0.01):
+    print('finite difference for 2nd Dx:')
+    return (np.sin(x+h)-2*np.sin(x)+np.sin(x-h))/(2*h**2)
+
+#Trapezoidal Rule of Integration
+
+def integration_sin(a=0,b=np.pi/3):
+    print('apprx integral of sin(x)')
+    return ((b-a)/2) * (np.sin(a) + np.sin(b)) # trapezoid rule
+
+print(second_derivative_sin(x))
+print(integration_sin())
